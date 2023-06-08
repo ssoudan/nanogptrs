0e6fe3d1f15b187244c19be8614cb283a036860e
Sebastien Soudan <sebastien.soudan@gmail.com>
2023-06-07 17:04:49 -0700

    feat: add tinystories dataset to download

--- data/download.sh
+++ data/download.sh
@@ -13,5 +13,7 @@ function download_if_not_exists() {
 mkdir -p $DIR/$g
 #download_if_not_exists https://github.com/matthewreagan/WebstersEnglishDictionary/blob/master/dictionary_compact.json $DIR/dictionary_compact.json
 download_if_not_exists https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt $DIR/input.txt
+#download_if_not_exists https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-train.txt $DIR/TinyStoriesV2-GPT4-train.txt
+#download_if_not_exists https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-valid.txt $DIR/TinyStoriesV2-GPT4-valid.txt
 
 echo "Done!"
